5c5a88479094320a093808a36493053739f8797c
Tejasri <tejasrimamidi@gmail.com>
2020-09-20 21:14:59 +0530

    worked done changed the function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const JsxElement = "I am a JSX element!";
+  const [bakery,setbakery] =useState({
+    cake :10,
+    icecream:20
+
+  })
+const giveNumber =(bakery)=>{
+   return bakery.cake + bakery.icecream
+}
+   const JsxWithChild = <div>
+    <h1>child-1</h1>
+    <h2>child-2</h2>
+   </div>
+   const name ="TejasriMamidi";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div>{JsxElement}{JsxWithChild}</div>
+    <div className="Hi">I am a div tag with class name as Hi.</div>
+  <div>I am {name}</div>
+  <div>Total number of cakes and icecreams in this bakery:{giveNumber(bakery)}</div>
     </div>
   );
 }
